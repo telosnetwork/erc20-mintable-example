cfd113f19068796f7d200c18fd6f96aafbebc8d5
Thomas Cuvillier <titiomm@gmail.com>
2022-06-15 22:21:36 +0200

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,9 +3,26 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
-    constructor(uint256 _initialSupply, string memory _name, string memory _symbol, address _owner) ERC20(_name, _symbol) {
-        _mint(_owner, _initialSupply * 10 ** decimals());
-    }
+contract Token is ERC20, Ownable {
+  
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
+  
+  bool public mintingFinished = false;
+    
+  constructor(uint256 _initialSupply, string memory _name, string memory _symbol, address _owner) ERC20(_name, _symbol) {}
+ 
+  function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+    _mint(_to, _amount * 10 ** decimals());
+    Mint(_to, _amount);
+    return true;
+  }
+ 
+  function finishMinting() external onlyOwner returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
 }
